da5a6ac22d07a0296f8f91ecc08d7c15db93bfef
Nik Erzetic <nik.erzetic@aflabs.com>
2025-03-04 08:51:13 +0100

    Working proof of reserves

--- contracts/proofOfReserves/ProofOfReserves.sol
+++ contracts/proofOfReserves/ProofOfReserves.sol
@@ -5,12 +5,14 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IEVMTransaction} from "@flarenetwork/flare-periphery-contracts/coston2/IEVMTransaction.sol";
 import {IJsonApi} from "@flarenetwork/flare-periphery-contracts/coston2/IJsonApi.sol";
 import {ContractRegistry} from "@flarenetwork/flare-periphery-contracts/coston2/ContractRegistry.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 struct DataTransportObject {
     uint256 reserves;
 }
 
-contract ProofOfReserves {
+contract ProofOfReserves is Ownable {
+    // Two events for debug purposes
     event GoodPair(address reader, address token, uint256 totalSupply);
     event BadPair(address reader, address token, uint256 totalSupply);
 
@@ -20,10 +22,16 @@ contract ProofOfReserves {
     uint256 public debugTokenReserves = 0;
     uint256 public debugClaimedReserves = 0;
 
-    constructor() {
+    constructor() Ownable(msg.sender) {
         // TODO make this dynamic with hardhat and Ownable
-        tokenStateReaders[0xa55ca3B48C3343f4eeC31433c4c9EEB6F806cF72] = 0x971C2CbD573e9aCbad555Fdd2252ab21eb73a962;
-        tokenStateReaders[0x3A8E713ca13ea199D3eB49b1bE13dcD04bB1F810] = 0x1C57e92ca1d10403B1F425699fe629B439F68A12;
+        // Coston
+        tokenStateReaders[0xA10896Efaa5719787B630997562d6637b30DeBc5] = 0x971C2CbD573e9aCbad555Fdd2252ab21eb73a962;
+        // Coston2
+        tokenStateReaders[0x91D1FA626B9555c045e1c9a00746aA621855ee01] = 0x1C57e92ca1d10403B1F425699fe629B439F68A12;
+    }
+
+    function updateAddress(address readerAddress, address tokenAddress) public onlyOwner {
+        tokenStateReaders[readerAddress] = tokenAddress;
     }
 
     function verifyReserves(IJsonApi.Proof calldata jsonProof, IEVMTransaction.Proof[] calldata transactionProofs)
@@ -56,7 +64,7 @@ contract ProofOfReserves {
         // TODO
         for (uint256 i = 0; i < proof.data.responseBody.events.length; i++) {
             IEVMTransaction.Event memory _event = proof.data.responseBody.events[i];
-            address readerAddress = proof.data.responseBody.sourceAddress;
+            address readerAddress = _event.emitterAddress;
             (address tokenAddress, uint256 supply) = abi.decode(_event.data, (address, uint256));
             bool correctTokenAndReaderAddress = tokenStateReaders[readerAddress] == tokenAddress;
             if (correctTokenAndReaderAddress) {
@@ -70,12 +78,10 @@ contract ProofOfReserves {
     }
 
     function isValidProof(IJsonApi.Proof calldata proof) private view returns (bool) {
-        // return true;
         return ContractRegistry.auxiliaryGetIJsonApiVerification().verifyJsonApi(proof);
     }
 
     function isValidProof(IEVMTransaction.Proof calldata proof) private view returns (bool) {
-        // return true;
         return ContractRegistry.getFdcVerification().verifyEVMTransaction(proof);
     }
 
