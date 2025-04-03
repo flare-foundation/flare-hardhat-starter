d7c1ce8eda781fe7e046c4af7993b151688493d4
Kristaps Grinbergs <kristaps@flare.network>
2025-04-03 16:26:26 +0300

    Refactor comments in FAssetsSettings contract and deployment script for clarity

--- contracts/fassets/FAssetsSettings.sol
+++ contracts/fassets/FAssetsSettings.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-// 1. Import the AssetManager interface
+// Import the AssetManager interface
 import { IAssetManager } from "@flarenetwork/flare-periphery-contracts/coston/IAssetManager.sol";
 
-// 2. Contract for accessing FAssets settings from the asset manager
+// Contract for accessing FAssets settings from the asset manager
 contract FAssetsSettings {
-  // 3. Connection to the AssetManager contract
+  // Connection to the AssetManager contract
   IAssetManager public assetManager;
 
-  // 4. Constructor initializes the contract with the AssetManager contract address
+  // Constructor initializes the contract with the AssetManager contract address
   constructor(address _assetManager) {
     assetManager = IAssetManager(_assetManager);
   }
 
-  // 5. This function gets two important numbers from the asset manager settings:
+  // This function gets two important numbers from the asset manager settings:
   // * lotSizeAMG: The smallest amount you can trade (in AMG units)
   // * assetDecimals: How many decimal places the asset uses
   // FAssets Operation Parameters https://dev.flare.network/fassets/operational-parameters
--- scripts/fassets/getLotSize.ts
+++ scripts/fassets/getLotSize.ts
@@ -1,26 +1,26 @@
 import { ethers } from "hardhat";
 import { FAssetsSettingsContract } from "../../typechain-types";
 
-// 1. AssetManager address on Songbird Testnet Coston network
+// AssetManager address on Songbird Testnet Coston network
 const ASSET_MANAGER_ADDRESS = "0x56728e46908fB6FcC5BCD2cc0c0F9BB91C3e4D34";
 
 async function main() {
   console.log("Deploying FAssetsSettings...");
 
-  //2.  Get the contract factory
+  //Get the contract factory
   const FAssetsSettings = await ethers.getContractFactory("FAssetsSettings") as FAssetsSettingsContract;
 
-  //3. Deploy the contract
+  //Deploy the contract
   const fAssetsSettings = await FAssetsSettings.deploy(ASSET_MANAGER_ADDRESS);
   await fAssetsSettings.waitForDeployment();
   console.log("FAssetsSettings deployed to:", await fAssetsSettings.getAddress());
 
-  // 4. Call getSettings function
+  //Call getSettings function
   const lotSize = await fAssetsSettings.getLotSize();
   console.log("Lot size:", lotSize[0]);
   console.log("Decimals:", lotSize[1]);
 
-  // 5. Convert lot size to XRP
+  //Convert lot size to XRP
   const lotSizeFXRP = Number(lotSize[0]) / Math.pow(10, Number(lotSize[1]));
   console.log("Lot size in XRP", lotSizeFXRP)
 }
