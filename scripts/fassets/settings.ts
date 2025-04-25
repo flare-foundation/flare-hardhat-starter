890f96f15385d535f764226d937e5d08d0a4daae
Kristaps Grinbergs <kristaps@flare.network>
2025-04-25 14:57:54 +0300

    Refactor settings script to use Truffle for contract interaction

--- scripts/fassets/settings.ts
+++ scripts/fassets/settings.ts
@@ -1,13 +1,13 @@
-import { ethers } from "hardhat";
-
 import { IAssetManagerInstance } from "../../typechain-types";
 
+const IAssetManager = artifacts.require("IAssetManager");
 const ASSET_MANAGER_ADDRESS = "0x56728e46908fB6FcC5BCD2cc0c0F9BB91C3e4D34";
 
 async function main() {
-  const assetManager: IAssetManagerInstance = await ethers.getContractAt("IAssetManager", ASSET_MANAGER_ADDRESS);
+  const assetManager = await IAssetManager.at(ASSET_MANAGER_ADDRESS) as IAssetManagerInstance;
   const settings = await assetManager.getSettings();
-  console.log("Settings: ", settings);
+
+  console.log(settings);
 }
 
 main().catch((error) => {
