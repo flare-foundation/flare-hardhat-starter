4525fd701263c4140736966f9f19e4f79b8e22a4
Kristaps Grinbergs <kristaps@flare.network>
2025-04-25 16:43:02 +0300

    Add deposit and withdraw scripts for WNAT functionality

---
+++ scripts/wnat/deposit.ts
@@ -0,0 +1,22 @@
+import { ethers } from "hardhat";
+import { IWNatInstance } from "../../typechain-types/@flarenetwork/flare-periphery-contracts/coston/IWNat";
+
+const WNAT_ADDRESS = "0xC67DCE33D7A8efA5FfEB961899C73fe01bCe9273";
+
+async function main() {
+  const IWNat = (await ethers.getContractAt(
+    "@flarenetwork/flare-periphery-contracts/coston2/IWNat.sol:IWNat",
+    WNAT_ADDRESS
+  )) as unknown as IWNatInstance;
+
+  // Deposit 0.1 ETH (or whatever amount you want)
+  const depositAmount = ethers.parseEther("0.1").toString();
+  const tx = await IWNat.deposit({ value: depositAmount });
+  console.log("Deposited 0.1 ETH to WNAT", tx);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
---
+++ scripts/wnat/withdraw.ts
@@ -0,0 +1,22 @@
+import { ethers } from "hardhat";
+import { IWNatInstance } from "../../typechain-types/@flarenetwork/flare-periphery-contracts/coston/IWNat";
+
+const WNAT_ADDRESS = "0xC67DCE33D7A8efA5FfEB961899C73fe01bCe9273";
+
+async function main() {
+  const IWNat = (await ethers.getContractAt(
+    "@flarenetwork/flare-periphery-contracts/coston2/IWNat.sol:IWNat",
+    WNAT_ADDRESS
+  )) as unknown as IWNatInstance;
+
+  const withdrawAmount = ethers.parseEther("0.1").toString();
+
+  const tx = await IWNat.withdraw(withdrawAmount);
+  console.log("Withdrew", tx);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
