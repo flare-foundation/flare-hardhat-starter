0a5d37dc5d4c73ba6b3faa375c7eeece2b87cc14
Kristaps Grinbergs <kristaps@flare.network>
2025-05-08 10:11:58 +0300

    Refactor WNAT deposit and withdraw scripts to dynamically retrieve contract address and enhance balance logging

--- scripts/wnat/withdraw.ts
+++ scripts/wnat/withdraw.ts
@@ -1,20 +1,38 @@
 import { ethers } from "hardhat";
-import { IWNatInstance } from "../../typechain-types/@flarenetwork/flare-periphery-contracts/coston/IWNat";
 
-const WNAT_ADDRESS = "0x767b25A658E8FC8ab6eBbd52043495dB61b4ea91";
+import { IWNatInstance } from "../../typechain-types/@flarenetwork/flare-periphery-contracts/coston/IWNat";
+import { IFlareContractRegistryInstance } from "typechain-types/@flarenetwork/flare-periphery-contracts/coston/IFlareContractRegistry";
+
+const FLARE_CONTRACTS_REGISTRY_ADDRESS = "0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019";
 
 const iWNatArtifact = artifacts.require("IWNat");
+const iFlareContractRegistryArtifact = artifacts.require("IFlareContractRegistry");
 
 
 // Run with command
 // npx hardhat run scripts/wnat/withdraw.ts --network coston
 async function main() {
-  const IWNatInstance: IWNatInstance = await iWNatArtifact.at(WNAT_ADDRESS);
+  const contractRegistry: IFlareContractRegistryInstance = await iFlareContractRegistryArtifact.at(FLARE_CONTRACTS_REGISTRY_ADDRESS);
+  const wnatAddress = await contractRegistry.getContractAddressByName("WNat");
+  console.log(wnatAddress);
+
+  const IWNatInstance: IWNatInstance = await iWNatArtifact.at(wnatAddress);
+
+  const accounts = await web3.eth.getAccounts();
+  const account = accounts[0];
+  console.log("Using account:", account);
+
+  // Get initial balance
+  const initialBalance = await IWNatInstance.balanceOf(account);
 
   // Withdraw 0.1 ETH
   const withdrawAmount = ethers.parseEther("0.1").toString();
   const tx = await IWNatInstance.withdraw(withdrawAmount);
-  console.log("Withdrew from WNAT to native token", tx);
+  const finalBalance = await IWNatInstance.balanceOf(account);
+
+  // console.log("Withdrew from WNAT to native token", tx);
+  console.log("Initial WNAT balance:", web3.utils.fromWei(initialBalance, 'ether'), "WNAT");
+  console.log("Final WNAT balance:", web3.utils.fromWei(finalBalance, 'ether'), "WNAT");
 }
 
 main().catch((error) => {
