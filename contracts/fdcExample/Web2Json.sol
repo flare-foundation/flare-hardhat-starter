fea3ce7a4a2c5a4f8c91d737af718c18c44ad18e
amadiaflare <anthony.madia@flare.network>
2025-05-24 11:53:10 -0400

    chore: default contracts to coston2

--- contracts/fdcExample/Web2Json.sol
+++ contracts/fdcExample/Web2Json.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import {ContractRegistry} from "@flarenetwork/flare-periphery-contracts/coston/ContractRegistry.sol";
-import {IWeb2Json} from "@flarenetwork/flare-periphery-contracts/coston/IWeb2Json.sol";
+import {ContractRegistry} from "@flarenetwork/flare-periphery-contracts/coston2/ContractRegistry.sol";
+import {IWeb2Json} from "@flarenetwork/flare-periphery-contracts/coston2/IWeb2Json.sol";
 
 struct StarWarsCharacter {
     string name;
