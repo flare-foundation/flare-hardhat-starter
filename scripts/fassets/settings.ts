4f51eb1ee944f710cc2351ea0b8003ff6767a945
Kristaps Grinbergs <kristaps@flare.network>
2025-06-02 16:21:54 +0300

    chore: update AssetManager address for Flare Testnet Coston2

--- scripts/fassets/settings.ts
+++ scripts/fassets/settings.ts
@@ -1,7 +1,11 @@
 import { IAssetManagerInstance } from "../../typechain-types";
 
+// yarn hardhat run scripts/fassets/settings.ts --network coston2
+
 const IAssetManager = artifacts.require("IAssetManager");
-const ASSET_MANAGER_ADDRESS = "0x56728e46908fB6FcC5BCD2cc0c0F9BB91C3e4D34";
+
+// AssetManager address on Flare Testnet Coston2 network
+const ASSET_MANAGER_ADDRESS = "0xDeD50DA9C3492Bee44560a4B35cFe0e778F41eC5";
 
 async function main() {
     const assetManager = (await IAssetManager.at(ASSET_MANAGER_ADDRESS)) as IAssetManagerInstance;
