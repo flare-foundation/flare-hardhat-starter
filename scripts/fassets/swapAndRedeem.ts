3817ac3d73ccf2739a7d2ef7688d5ce86226bd87
Kristaps Grinbergs <kristaps@flare.network>
2025-06-03 12:12:07 +0300

    chore: update addresses for AssetManager and BlazeSwap router on Flare Testnet Coston2

--- scripts/fassets/swapAndRedeem.ts
+++ scripts/fassets/swapAndRedeem.ts
@@ -3,15 +3,18 @@ import { run } from "hardhat";
 import { SwapAndRedeemInstance } from "../../typechain-types";
 import { ERC20Instance } from "../../typechain-types/@openzeppelin/contracts/token/ERC20/ERC20";
 
-// AssetManager address on Songbird Testnet Coston network
-const ASSET_MANAGER_ADDRESS = "0x56728e46908fB6FcC5BCD2cc0c0F9BB91C3e4D34";
+// yarn hardhat run scripts/fassets/swapAndRedeem.ts --network coston2
+
+// AssetManager address on Flare Testnet Coston2 network
+const ASSET_MANAGER_ADDRESS = "0xDeD50DA9C3492Bee44560a4B35cFe0e778F41eC5";
 const LOTS_TO_REDEEM = 1;
 const UNDERLYING_ADDRESS = "rSHYuiEvsYsKR8uUHhBTuGP5zjRcGt4nm";
-// BlazeSwap router address on Songbird Testnet Coston network
-const SWAP_ROUTER_ADDRESS = "0xf0D01450C037DB2903CF5Ff638Dd1e2e6B0EEDF4";
+
+// BlazeSwap router address on Flare Testnet Coston2 network
+const SWAP_ROUTER_ADDRESS = "0x8D29b61C41CF318d15d031BE2928F79630e068e6";
 const SWAP_PATH = [
-    "0x767b25A658E8FC8ab6eBbd52043495dB61b4ea91", // WCFLR
-    "0x36be8f2e1CC3339Cf6702CEfA69626271C36E2fd", // FXRP
+    "0xC67DCE33D7A8efA5FfEB961899C73fe01bCe9273", // WC2FLR
+    "0x8b4abA9C4BD7DD961659b02129beE20c6286e17F", // FXRP
 ];
 
 async function deployAndVerifyContract() {
