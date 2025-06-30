9a967a10d2fc7ede412751ec24d3924ccfe462c2
Kristaps Grinbergs <kristaps@flare.network>
2025-06-30 16:00:42 +0300

    feat: add AssetManagerRegistry contract and deployment script
    - Introduced AssetManagerRegistry contract to manage asset managers and retrieve FXRP asset manager address.
    - Added deployment script to initialize AssetManagerRegistry and log the FXRP asset manager address.

---
+++ contracts/fassets/AssetManagerRegistry.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IFlareContractRegistry} from "@flarenetwork/flare-periphery-contracts/coston2/IFlareContractRegistry.sol";
+
+import {IAssetManager} from "@flarenetwork/flare-periphery-contracts/coston2/IAssetManager.sol";
+import {IAssetManagerController} from "@flarenetwork/flare-periphery-contracts/coston2/IAssetManagerController.sol";
+
+import {ContractRegistry} from "@flarenetwork/flare-periphery-contracts/coston2/ContractRegistry.sol";
+
+import {AssetManagerSettings} from "@flarenetwork/flare-periphery-contracts/coston2/userInterfaces/data/AssetManagerSettings.sol";
+
+contract AssetManagerRegistry {
+
+    // TXRP_HASH is the hash of the string "TXRP"
+    bytes32 private constant TXRP_HASH = keccak256(abi.encodePacked("TXRP"));
+
+    function getFxrpAssetManager() public view returns (address) {
+        // Use the ContractRegistry library to get the AssetManagerController
+        IAssetManagerController assetManagerController = ContractRegistry
+            .getAssetManagerController();
+
+        // Get all the asset managers from the AssetManagerController
+        IAssetManager[] memory assetManagers = assetManagerController
+            .getAssetManagers();
+
+        // Iterate over the asset managers
+        for (uint256 i = 0; i < assetManagers.length; i++) {
+            IAssetManager assetManager = IAssetManager(assetManagers[i]);
+
+            // Get the settings of the asset manager
+            AssetManagerSettings.Data memory settings = assetManager
+                .getSettings();
+
+            //return the address of the asset manager that has the pool token suffix "TXRP"
+            if (
+                keccak256(abi.encodePacked(settings.poolTokenSuffix)) ==
+                TXRP_HASH
+            ) {
+                return address(assetManager);
+            }
+        }
+        // If no asset manager is found, return the zero address
+        return address(0);
+    }
+}
---
+++ scripts/fassets/getAssetManager.ts
@@ -0,0 +1,16 @@
+// yarn hardhat run scripts/fassets/getAssetManager.ts --network coston2
+
+async function main() {
+    const AssetManagerRegistry = artifacts.require("AssetManagerRegistry");
+
+    const assetManagerRegistry = await AssetManagerRegistry.new();
+    console.log("FAssetsSettings deployed to:", assetManagerRegistry.address);
+
+    const fxrpAssetManager = await assetManagerRegistry.getFxrpAssetManager();
+    console.log("FXRP Asset Manager address:", fxrpAssetManager);
+}
+
+main().catch(error => {
+    console.error(error);
+    process.exitCode = 1;
+});
