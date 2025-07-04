38509bf78ca798fa0d4c30fdaba2c40958b62026
Kristaps Grinbergs <kristaps@flare.network>
2025-07-04 16:11:57 +0300

    feat: revert getLot size script

--- scripts/fassets/getLotSize.ts
+++ scripts/fassets/getLotSize.ts
@@ -1,5 +1,8 @@
 // yarn hardhat run scripts/fassets/getLotSize.ts --network coston2
 
+// AssetManager address on Flare Testnet Coston2 network
+const ASSET_MANAGER_ADDRESS = "0xDeD50DA9C3492Bee44560a4B35cFe0e778F41eC5";
+
 async function main() {
     console.log("Deploying FAssetsSettings...");
 
@@ -7,7 +10,7 @@ async function main() {
     const FAssetsSettings = artifacts.require("FAssetsSettings");
 
     // Deploy the contract
-    const fAssetsSettings = await FAssetsSettings.new();
+    const fAssetsSettings = await FAssetsSettings.new(ASSET_MANAGER_ADDRESS);
     console.log("FAssetsSettings deployed to:", fAssetsSettings.address);
 
     // Call getSettings function
