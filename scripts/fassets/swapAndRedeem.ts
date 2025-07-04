e879286053845d5dc326c2a688a0e98608968aec
Kristaps Grinbergs <kristaps@flare.network>
2025-07-04 12:03:24 +0300

    refactor: update FAssets contracts to use FlareContractsRegistry for asset manager retrieval

--- scripts/fassets/swapAndRedeem.ts
+++ scripts/fassets/swapAndRedeem.ts
@@ -3,23 +3,27 @@ import { run } from "hardhat";
 import { SwapAndRedeemInstance } from "../../typechain-types";
 import { ERC20Instance } from "../../typechain-types/@openzeppelin/contracts/token/ERC20/ERC20";
 
+import { getFXRPAssetManagerAddress } from "./getFXRPAssetManagerAddress";
+
 // yarn hardhat run scripts/fassets/swapAndRedeem.ts --network coston2
 
-// AssetManager address on Flare Testnet Coston2 network
-const ASSET_MANAGER_ADDRESS = "0xDeD50DA9C3492Bee44560a4B35cFe0e778F41eC5";
 const LOTS_TO_REDEEM = 1;
 const UNDERLYING_ADDRESS = "rSHYuiEvsYsKR8uUHhBTuGP5zjRcGt4nm";
 
 // BlazeSwap router address on Flare Testnet Coston2 network
 const SWAP_ROUTER_ADDRESS = "0x8D29b61C41CF318d15d031BE2928F79630e068e6";
-const SWAP_PATH = [
-    "0xC67DCE33D7A8efA5FfEB961899C73fe01bCe9273", // WC2FLR
-    "0x8b4abA9C4BD7DD961659b02129beE20c6286e17F", // FXRP
-];
+const WC2FLR = "0xC67DCE33D7A8efA5FfEB961899C73fe01bCe9273";
+
+const IAssetManager = artifacts.require("IAssetManager");
 
 async function deployAndVerifyContract() {
+    const assetManagerAddress = await getFXRPAssetManagerAddress();
+    const assetManager = await IAssetManager.at(assetManagerAddress);
+    const fassetAddress = await assetManager.fAsset();
+    const swapPath = [WC2FLR, fassetAddress];
+
     const SwapAndRedeem = artifacts.require("SwapAndRedeem");
-    const args = [SWAP_ROUTER_ADDRESS, ASSET_MANAGER_ADDRESS, SWAP_PATH];
+    const args = [SWAP_ROUTER_ADDRESS, swapPath];
     const swapAndRedeem: SwapAndRedeemInstance = await SwapAndRedeem.new(...args);
 
     const fassetsSwapAndRedeemAddress = await swapAndRedeem.address;
@@ -50,7 +54,7 @@ async function main() {
 
     // Get WCFLR token
     const ERC20 = artifacts.require("ERC20");
-    const wcflr: ERC20Instance = await ERC20.at(SWAP_PATH[0]);
+    const wcflr: ERC20Instance = await ERC20.at(WC2FLR);
 
     const approveTx = await wcflr.approve(swapAndRedeemAddress, amountOut);
     console.log("Approve transaction: ", approveTx);
