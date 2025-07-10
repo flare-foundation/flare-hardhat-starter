c45c168497cdb631242117f747a37a122fc52dbe
Kristaps Grinbergs <kristaps@flare.network>
2025-07-10 13:17:25 +0300

    refactor: clean up TokenFaucet deployment script by removing unused imports and comments
    - Removed the IAssetManager import and related comments for clarity.
    - Simplified the script by eliminating the ASSET_MANAGER_ADDRESS constant, as it is no longer needed.

--- scripts/fassets/faucet/tokenFaucet.ts
+++ scripts/fassets/faucet/tokenFaucet.ts
@@ -1,13 +1,9 @@
 import { run } from "hardhat";
-import { TokenFaucetInstance, IAssetManagerInstance } from "../../../typechain-types";
+import { TokenFaucetInstance } from "../../../typechain-types";
 
 // yarn hardhat run scripts/fassets/faucet/tokenFaucet.ts --network coston2
 
 const TokenFaucet = artifacts.require("TokenFaucet");
-const IAssetManager = artifacts.require("IAssetManager");
-
-// AssetManager address on Flare Testnet Coston2 network
-const ASSET_MANAGER_ADDRESS = "0xDeD50DA9C3492Bee44560a4B35cFe0e778F41eC5";
 
 async function main() {
     const tokenFaucet: TokenFaucetInstance = await TokenFaucet.new();
