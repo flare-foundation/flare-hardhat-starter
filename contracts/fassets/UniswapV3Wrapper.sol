232fa377abb086ea1f7a7b025b5c95efee7d4c8e
Kristaps Grinbergs <kristaps@flare.network>
2025-10-20 15:59:21 +0300

    Remove ExactInputParams struct from UniswapV3Wrapper to simplify interface

--- contracts/fassets/UniswapV3Wrapper.sol
+++ contracts/fassets/UniswapV3Wrapper.sol
@@ -17,16 +17,7 @@ interface ISwapRouter {
         uint160 sqrtPriceLimitX96;
     }
 
-    struct ExactInputParams {
-        bytes path;
-        address recipient;
-        uint256 deadline;
-        uint256 amountIn;
-        uint256 amountOutMinimum;
-    }
-
     function exactInputSingle(ExactInputSingleParams calldata params) external payable returns (uint256 amountOut);
-    function exactInput(ExactInputParams calldata params) external payable returns (uint256 amountOut);
     function factory() external view returns (address);
 }
 
